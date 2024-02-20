05c49235987c60e715a09d034e70c3acb7cdcd76
Vlad <v.katk0ov@gmail.com>
2024-02-20 11:56:43 +0200

    first load

---
+++ js/index.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".header__burger").click(function(e){$(".header__burger").toggleClass("active"),$(".header__menu").toggleClass("active")}),$(".slider").slick({arrows:!1,dots:!0,slidesToShow:3,slidesToScroll:2,autoplay:!0,autoplaySpeed:3e3})}),$(window).resize(function(){console.log($(window).width()+"x"+$(window).height())});
